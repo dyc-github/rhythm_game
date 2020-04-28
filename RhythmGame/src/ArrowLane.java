55e43227b0e66917a4ac3cda0f0d55e5b1d59ccb
Ryan_Tai <Ryan_Tai@DESKTOP-US8GGMC.attlocal.net>
2020-04-27 20:09:20 -0700

    Added the Arrow Class, RhythmGame Class and made an empty Music class so the game wouldn't break. Also edited the ArrowLane Class adding in dummy variables to prevent error. The code seems to work fine with falling arrows, however during the first couple of times the velocity of the arrows seemed inconsistent but the problem seemed to have disappeared somehow.

--- RhythmGame/src/ArrowLane.java
+++ RhythmGame/src/ArrowLane.java
@@ -2,59 +2,93 @@ import java.awt.Graphics2D;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class ArrowLane {
-	
-	private final int xPos;
-	private static final int goal =13;//change later
-	private LinkedList<Arrow> arrows;
-	private final Direction direction;
 
-	public ArrowLane(int xPos, Direction direction) {
-		this.direction = direction;
-		this.xPos= xPos;
-	}
-	
-	public void add(Arrow arrow) {
-		arrows.add(arrow);
-	}
-	
-	public int compare() {
-		if (arrows.isEmpty()){
-			return 0;
-		}
-		Arrow arrow = arrows.peek();
-		int diff = Math.abs(arrow.getY() - goal);
-		
-		if (arrow.getY() > screensize + half arrow height) // TODO define screensize and half arrow height
-		
-		if (diff <= perfect) { //TODO define perfect
-			return 3; // FYI 3 is best
-		}
-		else if (diff <= ok) { // TODO define ok
-			return 2;
-		}
-		else if (diff <= bad) { // TODO defien bad
-			return 1;
-		}
-		else {
-			return 0;	
-		}
-	}
-	
-	public void remove() {
-		arrows.remove();
-	}
-	
-	public int getX() {
-		return xPos;
-	}
-	
-	public void draw(Graphics2D g2) {
-		Iterator<Arrow> arrowsIterator = arrows.descendingIterator();
-		while(arrowsIterator.hasNext()) {
-			Arrow arrow = arrowsIterator.next();
-			arrow.draw(g2, direction);
-		}
-	}
+public class ArrowLane
+{
+
+    private final int xPos;
+
+    private static final int goal = 13;// change later
+
+    private LinkedList<Arrow> arrows;
+
+    private final Direction direction;
+
+
+    public ArrowLane( int xPos, Direction direction )
+    {
+        this.direction = direction;
+        this.xPos = xPos;
+        arrows = new LinkedList<Arrow>();
+    }
+
+
+    public void add()
+    {
+        arrows.add( new Arrow( this ) );
+    }
+
+
+    public int compare()
+    {
+        if ( arrows.isEmpty() )
+        {
+            return 0;
+        }
+        Arrow arrow = arrows.peek();
+        int diff = Math.abs( arrow.getY() - goal );
+
+        // TODO define screensize and half arrow height
+
+        if ( diff <= 10 )
+        { // TODO define perfect
+            return 3; // FYI 3 is best
+        }
+        else if ( diff <= 25 )
+        { // TODO define ok
+            return 2;
+        }
+        else if ( diff <= 50 )
+        { // TODO defien bad
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+    public void remove()
+    {
+        arrows.remove();
+    }
+
+
+    public int getX()
+    {
+        return xPos;
+    }
+
+
+    public void move()
+    {
+        for ( Arrow a : arrows )
+        {
+            a.move();
+        }
+    }
+
+
+    public void draw( Graphics2D g2 )
+    {
+        Iterator<Arrow> arrowsIterator = arrows.descendingIterator();
+        System.out.println( "arrowLane draw" );
+        while ( arrowsIterator.hasNext() )
+        {
+            Arrow arrow = arrowsIterator.next();
+            arrow.draw( g2, direction );
+        }
+    }
 
 }
--- RhythmGame/src/RhythmGame.java
+++ RhythmGame/src/RhythmGame.java
@@ -1,6 +1,99 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class RhythmGame {
-	public static void main(String noargs[]) {
-		System.out.println("test");
-	}
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class RhythmGame extends JPanel implements ActionListener
+{
+    private ArrowLane[] lanes;
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT =1000;
+    private int worldTime;
+    private Timer timer;
+    private String pathnameFile;
+    private Scanner scanner;
+    private String currentLine;
+    private PrintWriter output;
+    private MusicPlayer  musicPlayer;
+    private JFrame window;
+    
+    public RhythmGame() {
+        window = new JFrame();
+        constructJPanel( window );
+        timer = new Timer(5,this);
+        lanes = new ArrowLane[4];
+        Direction[] dir = {Direction.LEFT, Direction.DOWN, Direction.UP, Direction.RIGHT};
+        for (int i = 0; i<4;i++) {
+            lanes[i] = new ArrowLane(100+200*i, dir[i]);
+        }
+        for (int i = 0; i<4;i++) {
+            lanes[i].add();
+        }
+        startPlay(null); //Remove this after we add file selection
+        //SetWorldTime to a negative number depending on velocity
+    }
+    
+    private void constructJPanel( JFrame window2 )
+    {
+        window.add( this );
+        window.setVisible( true );
+        window.setSize( SCREEN_WIDTH + 20, SCREEN_HEIGHT );
+        window.setTitle( "Rhythm Game" );
+        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+    }
+    
+    public ArrowLane getArrowLane(int direction) {
+        if (direction<lanes.length-1) {
+            return lanes[direction];
+        }
+        return null;
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent( g );
+        Graphics2D g2 = (Graphics2D)g;
+        for (ArrowLane al: lanes) {
+            al.draw( g2 );
+        }
+        //draw the lines
+    }
+    
+    
+    @Override
+    public void actionPerformed( ActionEvent arg0 )
+    {
+        for (ArrowLane al: lanes) {
+            al.move();
+        }
+        repaint();   
+    }
+    
+    public void startPlay(String pathname) {
+        timer.start();
+    }
+    
+    public void startRecord(String pathname) {
+        
+    }
+    
+    public void record(String direction) {
+        
+    }
+    
+    public static int getScreenHeight() {
+        return SCREEN_HEIGHT;
+    }
+    public static void main( String[] args )
+    {
+        RhythmGame game  = new RhythmGame();
+
+    }
+
 }
