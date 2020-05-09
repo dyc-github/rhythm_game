11fd2dcbc3fc7487a2568a64f076d679a3d87a34
David <davidpumpkin1@gmail.com>
2020-05-09 01:29:03 -0700

    wrapping up gui and recording as well a leaderboard

---
+++ RhythmGame/src/WindowChangeListener.java
@@ -0,0 +1,8 @@
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class WindowChangeListener extends ComponentAdapter{
+	public void componentResized(ComponentEvent e) {
+		
+	}
+}
